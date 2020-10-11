b3eda165dcf3d501e533e74fa353a5684a087810
Aleksandr Kostenko <avk076@gmail.com>
2020-10-11 20:13:19 +0300

    14 lesson end

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import classes from './NavBar.module.css'
 const NavBar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}>
+            <div className={`${classes.item} ${classes.active}`}>
                 <a> Profile</a>
             </div>
             <div className={classes.item}>
