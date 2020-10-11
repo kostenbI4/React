e45dfc7b59c2318f5900c23673c4dde400cf6a86
Aleksandr Kostenko <avk076@gmail.com>
2020-10-11 11:28:21 +0300

    Урок 14

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div className={'fon'}>
+                <img src={'http://www.hqwallpapers.ru/wallpapers/nature/serye-tuchi.jpg'}/>
+            </div>
+            <div className={'logo'}>
+                <img
+                    src={'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}/>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
